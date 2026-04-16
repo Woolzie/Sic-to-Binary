e611242afa44317fe13e78bd67f7ad09ac1d3214
Woolzie <snana.swag@gmail.com>
2026-04-16 15:23:41 +0530

    fix: alginment of labels and cells

--- utils/table.go
+++ utils/table.go
@@ -13,25 +13,50 @@ func Table(labels []string, cellSize []int , binary string){
 		return
 	}
 
-	labelPadding, sizePadding := makePadding(labels, cellSize)
+	labelPadding, cellPadding := makePadding(labels, cellSize)
 
+	//NOTE: this is a hacky fix to make the labels algin with the cells
+	fmt.Printf(" ")
+	if len(labels) > 3{
+		fmt.Printf(" ")
+	}
 	for i:= range labels{
 		pad := strings.Repeat(" ", labelPadding[i])
-    fmt.Printf(" %s%s%s", pad, labels[i], pad)
+    fmt.Printf("%s%s%s ", pad, labels[i], pad)
 	}
 	fmt.Println()
 
 	left := 0
 	right:= 0
+	cellBegin:= 0
 
 	//TODO: add space between each half byte (for readability)
 
+	//INFO: printing within the cell
 	for i:= range cellSize{
-		pad := strings.Repeat(" ", sizePadding[i])
 
-		right += cellSize[i]
-		fmt.Printf("|%s%s%s", pad, binary[left: right ], pad)
-		left = right
+		pad := strings.Repeat(" ", cellPadding[i])
+		fmt.Printf("|%s", pad)
+
+		// prints the half byte value and space repeatedly
+    for j:= cellSize[i]/4; j>=0; j--{
+
+			right += 4
+      if right - cellBegin > cellSize[i]{
+        right = cellSize[i] + cellBegin
+			}
+
+			fmt.Printf("%s", binary[left:right])
+			if j > 0 {
+				fmt.Printf(" ")
+			}
+
+			left = right
+		}
+
+		fmt.Printf("%s", pad)
+		cellBegin+= cellSize[i]
+
 	}
 	fmt.Println("|")
 	fmt.Println()
@@ -48,8 +73,7 @@ func makePadding(labels []string, cellSize []int) ( []int, []int ){
 		labelSize := len(labels[i])
 
 		//TODO: dont change this logic here, trust
-		// spaces := ( cellSize[i] / 4 )*2 //two spaces for seperation
-		spaces :=0
+		spaces := ( cellSize[i] / 4 ) //two spaces for seperation
 
 		if labelSize < cellSize[i] {
 			labelPadding[i] = ( cellSize[i] + spaces - labelSize )/2 + 2 
